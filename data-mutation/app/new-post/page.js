dce260b1a167822912207608aef77e322e42ec67
sina koushki <sinaderik7@gmail.com>
2025-03-15 19:49:35 +0330

    loading status added to the form

--- data-mutation/app/new-post/page.js
+++ data-mutation/app/new-post/page.js
@@ -1,17 +1,20 @@
+import FormSubmit from "@/components/form-submit";
 import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
 
 export default function NewPostPage() {
   async function createPost(formData) {
     "use server";
-    const title = formData.get('title');
-    const content = formData.get('content');
+    const title = formData.get("title");
+    const content = formData.get("content");
 
     storePost({
-      imageUrl: '',
+      imageUrl: "",
       title,
       content,
-      userId: 1
-    })
+      userId: 1,
+    });
+    redirect("/feed")
   }
 
   return (
@@ -36,8 +39,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+          <FormSubmit />
         </p>
       </form>
     </>
