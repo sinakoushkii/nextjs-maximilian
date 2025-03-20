27d02ada33217d35963f820c7e41d0a505a8e920
sina koushki <sinaderik7@gmail.com>
2025-03-20 19:50:17 +0330

    lucia instance initilized

---
+++ auth/lib/auth.js
@@ -0,0 +1,17 @@
+const { BetterSqlite3Adapter } = require("@lucia-auth/adapter-sqlite");
+import { Lucia } from "lucia";
+import db from "./db";
+
+const adapter = new BetterSqlite3Adapter(db, {
+  user: "users",
+  session: "sessions",
+});
+
+const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
