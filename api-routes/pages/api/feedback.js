cda077785d255fef5da70b9286535d5a0b456884
sina koushki <sinaderik7@gmail.com>
2025-03-26 20:47:57 +0330

    feedback api route created to handle post reqs

--- api-routes/pages/api/feedback.js
+++ api-routes/pages/api/feedback.js
@@ -1,5 +1,27 @@
-function handler(req,res){
-    res.status(200).json({message: 'This is a feedback API route'});
+import fs from "fs";
+import path from "path";
+
+function handler(req, res) {
+  if (req.method === "post") {
+    const email = req.body.email;
+    const feedback = req.body.feedback;
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      feedback: feedback,
+    };
+
+    const filePath = path.join(process.cwd(), "data", "feedback.json");
+    const fileData = fs.readFileSync(filePath);
+    const data = JSON.parse(fileData);
+    
+    data.push(newFeedback);
+
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: "Success !", feedback: newFeedback });
+  } else {
+    res.status(200).json({ message: "This is a feedback API route" });
+  }
 }
 
 export default handler;
--- api-routes/pages/index.js
+++ api-routes/pages/index.js
@@ -1,7 +1,31 @@
+import {useRef} from "react"
+
 function HomePage() {
+  const emailRef=useRef();
+  const feedbackRef=useRef();
+
+  function submitFormHandler(event){
+    event.preventDefault();
+
+    const enteredEmail=emailRef.current.value;
+    const enteredFeedback=feedbackRef.current.value;
+
+  }
+  
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input ref={emailRef} type="email" id="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your email feedback</label>
+          <textArea ref={feedbackRef} id="feedback" name="feedback" rows="5"></textArea>
+        </div>
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
